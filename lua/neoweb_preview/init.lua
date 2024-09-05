029bf1945de9e583f1222963d72cca33b736d03b
cory <coryj@gmx.com>
2024-09-05 15:48:09 -0600

    working

--- lua/neoweb_preview/init.lua
+++ lua/neoweb_preview/init.lua
@@ -1,43 +1,34 @@
-local pegasus = require("pegasus")
-local files = require("neoweb_preview.plugins.static")
 local config = require("neoweb_preview.config")
-local async = require("plenary.async")
+local llthreads2 = require("llthreads2")
+local task = require("neoweb_preview.task")
 
 local M = {}
 
-M.server = nil
-M.kill = false
+llthreads2.set_logger(function(msg) end)
+local running = false
 
 M.setup = function(user_config)
 	config.validate(user_config)
+	vim.api.nvim_create_user_command("NeowebPreviewStart", function()
+		require("neoweb_preview").run()
+	end, {})
+
+	vim.api.nvim_create_user_command("NeowebPreviewStop", function()
+		require("neoweb_preview").stop()
+	end, {})
 end
 
 M.run = function()
-	if M.server ~= nil then
-		M.server = pegasus:new({
-			port = config.config.port,
-			plugins = {
-				files:new({
-					location = vim.fn.getcwd(),
-				}),
-			},
-		})
-		M.server:start(function(request, response)
-			print("Do Server Stuff")
-			if M.kill then
-				return true
-			end
-		end)
+	if not running then
+		running = true
+		local edit = task:gsub("{{port}}", config.config.port):gsub("{{root}}", '"' .. vim.fn.getcwd() .. '"')
+		local thread = llthreads2.new(edit)
+		thread:start(true, true)
 	end
 
 	local cwd = vim.fn.getcwd()
 	local file_name = vim.api.nvim_buf_get_name(0):gsub(cwd, "")
 	vim.fn.jobstart(config.config.browser .. " http://0.0.0.0:" .. config.config.port .. "/" .. file_name)
-end
-
-M.stop = function()
-	M.kill = true
-	M.server = nil
 end
 
 return M
