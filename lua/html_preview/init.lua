913b8fa0d0dd3d3e97325810348523b0d58f1ac7
cory <coryj@gmx.com>
2024-09-27 10:47:37 -0600

    setup call

--- lua/html_preview/init.lua
+++ lua/html_preview/init.lua
@@ -1,12 +1,13 @@
 local server = require("html_preview.server")
 local vim = vim
+local M = {}
 
-local instances = {}
-local page = {}
-local stylebuf = {}
-local scriptbuf = {}
+M.instances = {}
+M.page = {}
+M.stylebuf = {}
+M.scriptbuf = {}
 
-vim.api.nvim_create_user_command("HTPreview", function(opts)
+M.setup = function()
 	local browser = ""
 
 	local OS = vim.loop.os_uname().sysname
@@ -18,115 +19,120 @@ vim.api.nvim_create_user_command("HTPreview", function(opts)
 		browser = "xdg-open"
 	end
 
-	local ext = vim.fn.expand("%:e")
-	if ext ~= "html" and ext ~= "htm" then
-		print("File is not html")
-	else
-		local bufnr = vim.api.nvim_get_current_buf()
-		local cwd = vim.fn.getcwd()
-		local css_files = {}
-		local js_files = {}
+	vim.api.nvim_create_user_command("HTMLPreview", function(opts)
+		local ext = vim.fn.expand("%:e")
+		if ext ~= "html" and ext ~= "htm" then
+			print("File is not html")
+		else
+			local bufnr = vim.api.nvim_get_current_buf()
+			local cwd = vim.fn.getcwd()
+			local css_files = {}
+			local js_files = {}
 
-		stylebuf[cwd] = {}
-		scriptbuf[cwd] = {}
+			M.stylebuf[cwd] = {}
+			M.scriptbuf[cwd] = {}
 
-		if instances[cwd] then
-			if instances[cwd]:running() then
-				print("Server already running")
-				return
-			else
-				instances[cwd] = nil
-			end
-		end
-		instances[cwd] = server:new()
-		instances[cwd]:start(cwd, OS, opts.fargs[1])
-
-		--logic for starting from random buffer <---------------
-		if instances[cwd].websock_client == nil then
-			vim.fn.jobstart(browser .. " http://127.0.0.1:" .. instances[cwd]:getPort() .. "/neoweb.html")
-		end
-
-		do
-			page[cwd] = table.concat(vim.api.nvim_buf_get_lines(bufnr, 0, -1, false))
-
-			for lines in page[cwd]:gmatch("%a+%.css") do
-				table.insert(css_files, lines)
-			end
-
-			for lines in page[cwd]:gmatch("%a+%.js") do
-				table.insert(js_files, lines)
-			end
-
-			local timer = vim.uv.new_timer()
-			timer:start(500, 5, function()
-				if instances[cwd].websock_client then
-					instances[cwd]:ws_send("UPDA" .. page[cwd])
-					timer:stop()
+			if M.instances[cwd] then
+				if M.instances[cwd]:running() then
+					print("Server already running")
+					return
+				else
+					M.instances[cwd] = nil
 				end
-			end)
-		end
-
-		local send = function()
-			local sendpage = page[cwd]
-			for key, styledata in pairs(stylebuf[cwd]) do
-				sendpage = string.gsub(sendpage, "<link (.-)" .. key .. "(.-)>", styledata)
 			end
-			for key, scriptdata in pairs(scriptbuf[cwd]) do
-				sendpage = string.gsub(sendpage, "<script (.-)" .. key .. "(.-)</script>", scriptdata)
+			M.instances[cwd] = server:new()
+			M.instances[cwd]:start(cwd, OS, opts.fargs[1])
+
+
+			--logic for starting from random buffer <---------------
+			if M.instances[cwd].websock_client == nil then
+				vim.fn.jobstart(browser .. " http://127.0.0.1:" .. M.instances[cwd]:getPort() .. "/neoweb.html")
 			end
 
-			instances[cwd]:ws_send("UPDA" .. sendpage)
+			do
+				M.page[cwd] = table.concat(vim.api.nvim_buf_get_lines(bufnr, 0, -1, false))
+
+				for lines in M.page[cwd]:gmatch("%a+%.css") do
+					table.insert(css_files, lines)
+				end
+
+				for lines in M.page[cwd]:gmatch("%a+%.js") do
+					table.insert(js_files, lines)
+				end
+
+				local timer = vim.uv.new_timer()
+				timer:start(500, 5, function()
+					if M.instances[cwd].websock_client then
+						M.instances[cwd]:ws_send("UPDA" .. M.page[cwd])
+						timer:stop()
+					end
+				end)
+			end
+
+			local send = function()
+				local sendpage = M.page[cwd]
+				for key, styledata in pairs(M.stylebuf[cwd]) do
+					sendpage = string.gsub(sendpage, "<link (.-)" .. key .. "(.-)>", styledata)
+				end
+				for key, scriptdata in pairs(M.scriptbuf[cwd]) do
+					sendpage = string.gsub(sendpage, "<script (.-)" .. key .. "(.-)</script>", scriptdata)
+				end
+
+				M.instances[cwd]:ws_send("UPDA" .. sendpage)
+			end
+
+			vim.api.nvim_create_augroup("Neoweb-" .. cwd, { clear = true })
+
+			vim.api.nvim_create_autocmd({ "TextChanged", "TextChangedI" }, {
+				group = "Neoweb-" .. cwd,
+				buffer = bufnr,
+				callback = function()
+					M.page[cwd] = table.concat(vim.api.nvim_buf_get_lines(bufnr, 0, -1, false))
+					send()
+				end,
+			})
+
+			vim.api.nvim_create_autocmd({ "TextChanged", "TextChangedI" }, {
+				group = "Neoweb-" .. cwd,
+				pattern = js_files,
+				callback = function()
+					M.scriptbuf[cwd][vim.fn.expand("%:t")] = "<script>"
+						.. table.concat(vim.api.nvim_buf_get_lines(vim.api.nvim_get_current_buf(), 0, -1, false))
+						.. " </script>"
+					send()
+				end,
+			})
+
+			vim.api.nvim_create_autocmd({ "TextChanged", "TextChangedI" }, {
+				group = "Neoweb-" .. cwd,
+				pattern = css_files,
+				callback = function()
+					M.stylebuf[cwd][vim.fn.expand("%:t")] = "<style> "
+						.. table.concat(vim.api.nvim_buf_get_lines(vim.api.nvim_get_current_buf(), 0, -1, false))
+						.. " </style>"
+					send()
+				end,
+			})
+
+			vim.api.nvim_create_autocmd({ "BufDelete", "BufUnload", "BufWipeout" }, {
+				group = "Neoweb-" .. cwd,
+				buffer = bufnr,
+				callback = function()
+					M.instances[cwd]:ws_send("STOP")
+				end,
+			})
 		end
+	end, { nargs = '?' })
 
-		vim.api.nvim_create_augroup("Neoweb-" .. cwd, { clear = true })
+	-- Manually Stop Server
+	vim.api.nvim_create_user_command("HTMLPreviewStop", function()
+		local cwd = vim.fn.getcwd()
+		if M.instances[cwd] then
+			M.instances[cwd]:ws_send("STOP")
+		else
+			print("No server running")
+		end
+	end, {})
+end
 
-		vim.api.nvim_create_autocmd({ "TextChanged", "TextChangedI" }, {
-			group = "Neoweb-" .. cwd,
-			buffer = bufnr,
-			callback = function()
-				page[cwd] = table.concat(vim.api.nvim_buf_get_lines(bufnr, 0, -1, false))
-				send()
-			end,
-		})
-
-		vim.api.nvim_create_autocmd({ "TextChanged", "TextChangedI" }, {
-			group = "Neoweb-" .. cwd,
-			pattern = js_files,
-			callback = function()
-				scriptbuf[cwd][vim.fn.expand("%:t")] = "<script>"
-					.. table.concat(vim.api.nvim_buf_get_lines(vim.api.nvim_get_current_buf(), 0, -1, false))
-					.. " </script>"
-				send()
-			end,
-		})
-
-		vim.api.nvim_create_autocmd({ "TextChanged", "TextChangedI" }, {
-			group = "Neoweb-" .. cwd,
-			pattern = css_files,
-			callback = function()
-				stylebuf[cwd][vim.fn.expand("%:t")] = "<style> "
-					.. table.concat(vim.api.nvim_buf_get_lines(vim.api.nvim_get_current_buf(), 0, -1, false))
-					.. " </style>"
-				send()
-			end,
-		})
-
-		vim.api.nvim_create_autocmd({ "BufDelete", "BufUnload", "BufWipeout" }, {
-			group = "Neoweb-" .. cwd,
-			buffer = bufnr,
-			callback = function()
-				instances[cwd]:ws_send("STOP")
-			end,
-		})
-	end
-end, { nargs = "?" })
-
--- nually Stop Server
-vim.api.nvim_create_user_command("HTPreviewStop", function()
-	local cwd = vim.fn.getcwd()
-	if instances[cwd] then
-		instances[cwd]:ws_send("STOP")
-	else
-		print("No server running")
-	end
-end, {})
+return M
